db81292ea580de93d21c2224e52d83bb1ed4b458
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-11-29 01:53:05 +0300

    Refactoring main.py namely one func for converting data from csv to sql instead of three

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,5 +1,4 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
-import csv
 import os
 from pathlib import Path
 
@@ -18,9 +17,15 @@ def variables_for_convert_data_from_csv_to_sql():
         'path_customers_data': Path(Path(__file__).parent, 'north_data', 'customers_data.csv'),
         'path_employees_data': Path(Path(__file__).parent, 'north_data', 'employees_data.csv'),
         'path_orders_data': Path(Path(__file__).parent, 'north_data', 'orders_data.csv'),
-        'name_execute_customers_data': 'INSERT INTO customers VALUES (%s, %s, %s)',
-        'name_execute_employees_data': 'INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)',
-        'name_execute_orders_data': 'INSERT INTO orders VALUES (%s, %s, %s, %s, %s)',
+        'query': """
+                COPY %s FROM STDIN WITH
+                CSV
+                HEADER
+                DELIMITER AS ','        
+        """,
+        'name_database_customers': 'customers',
+        'name_database_employees': 'employees',
+        'name_database_orders': 'orders'
     }
 
 
@@ -39,62 +44,39 @@ VARIABLES_CONV = variables_for_convert_data_from_csv_to_sql()
 CONN = connect_to_database()
 
 
-def convert_customers_data_from_csv_to_sql(path_csv, name_execute: str) -> None:
-    """Convert Customers data from csv to SQL database."""
+def convert_data_from_csv_to_sql(path_csv, query, database) -> None:
+    """Convert data from csv to SQL database."""
 
     with CONN:
         with CONN.cursor() as cur:
             #  Read csv from a file
             with open(path_csv, newline='', encoding='utf-8') as f:
-                reader = csv.reader(f)
-                next(reader)  # without title table
-                for row in reader:
-                    cur.execute(name_execute, (row[0], row[1], row[2]))
-
-
-def convert_employees_data_from_csv_to_sql(path_csv, name_execute: str) -> None:
-    """Convert Employees data from csv to SQL database."""
-
-    with CONN:
-        with CONN.cursor() as cur:
-            #  Read csv from a file
-            with open(path_csv, newline='', encoding='utf-8') as f:
-                reader = csv.reader(f)
-                next(reader)  # without title table
-                for row in reader:
-                    cur.execute(name_execute, (row[0], row[1], row[2], row[3], row[4], row[5]))
-
-
-def convert_orders_data_from_csv_to_sql(path_csv, name_execute: str) -> None:
-    """Convert Orders data from csv to SQL database."""
-
-    with CONN:
-        with CONN.cursor() as cur:
-            #  Read csv from a file
-            with open(path_csv, newline='', encoding='utf-8') as f:
-                reader = csv.reader(f)
-                next(reader)  # without title table
-                for row in reader:
-                    cur.execute(name_execute, (row[0], row[1], row[2], row[3], row[4]))
+                cur.copy_expert(sql=query % database, file=f)
 
 
 def main() -> None:
     """Run converting data from csv to SQL."""
 
     try:
-        convert_customers_data_from_csv_to_sql(
+        # Convert Customers data from csv to SQL database.
+        convert_data_from_csv_to_sql(
             path_csv=VARIABLES_CONV['path_customers_data'],
-            name_execute=VARIABLES_CONV['name_execute_customers_data']
+            query=VARIABLES_CONV['query'],
+            database=VARIABLES_CONV['name_database_customers']
         )
 
-        convert_employees_data_from_csv_to_sql(
+        # Convert Employees data from csv to SQL database.
+        convert_data_from_csv_to_sql(
             path_csv=VARIABLES_CONV['path_employees_data'],
-            name_execute=VARIABLES_CONV['name_execute_employees_data']
+            query=VARIABLES_CONV['query'],
+            database=VARIABLES_CONV['name_database_employees']
         )
 
-        convert_orders_data_from_csv_to_sql(
+        # Convert Orders data from csv to SQL database.
+        convert_data_from_csv_to_sql(
             path_csv=VARIABLES_CONV['path_orders_data'],
-            name_execute=VARIABLES_CONV['name_execute_orders_data']
+            query=VARIABLES_CONV['query'],
+            database=VARIABLES_CONV['name_database_orders']
         )
 
     finally:
