0c00ef3f9e209bad5b5a588868638e7ae8242133
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-12-04 00:42:07 +0300

    Completed the fourth exercise in homework.

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -45,7 +45,7 @@ WHERE
 			OR
 			category_name = 'Condiments'
 	)
-ORDER BY prod.units_in_stock
+ORDER BY prod.units_in_stock;
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
@@ -54,7 +54,15 @@ FROM customers
 WHERE customer_id NOT IN (
 	SELECT customer_id
 	FROM orders
-	)
+	);
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+
+SELECT product_name
+FROM products
+WHERE product_id IN (
+	SELECT product_id
+	FROM order_details
+	WHERE quantity = 10
+	);
