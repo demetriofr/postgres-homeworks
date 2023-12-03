b35ad55b8ddc9a85fa0b567097b1dda8346476a2
Dmitriy Frolov <dmitriy.a.frolov@gmail.com>
2023-12-04 00:32:43 +0300

    Completed the third exercise in homework.

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -49,6 +49,12 @@ ORDER BY prod.units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
 
+SELECT company_name
+FROM customers
+WHERE customer_id NOT IN (
+	SELECT customer_id
+	FROM orders
+	)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
